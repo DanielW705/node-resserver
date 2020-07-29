7b25c8c3291cf901bf14f574c97afbe5c9b32f1d
Daniel <danielmtzlpz95@gmail.com>
2020-07-29 12:56:05 -0500

    Mejorado con variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -7,8 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/prueba";
 } else {
-  urlDB =
-    "mongodb+srv://daniel:O0Nk2SXOaJ0gcJvv@cluster0.5hrqi.mongodb.net/cafe?retryWrites=true&w=majority";
+  urlDB = process.env.MONGO_URI;
 }
 process.env.urlDB = urlDB;
 //mongodb+srv://daniel:O0Nk2SXOaJ0gcJvv@cluster0.5hrqi.mongodb.net/cafe?retryWrites=true&w=majority
